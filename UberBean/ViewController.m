fc535cad133cc40c7e4071f146fa1dc468120728
Bennett <bennettcheung@yahoo.com>
2018-08-17 13:44:53 -0700

    App is able to get the user's location and show on the map

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -7,9 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
+
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) CLLocationManager* locationManager;
+@property (nonatomic, strong) CLLocation* location;
 @end
 
 @implementation ViewController
@@ -17,12 +22,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+
+    
+    [self CurrentLocationIdentifier];
+
 }
 
+-(void)CurrentLocationIdentifier
+{
+    //---- For getting current gps location
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    self.locationManager.distanceFilter = 20;
+    self.locationManager.delegate = self;
+    [self.locationManager requestWhenInUseAuthorization];
+    
+    //------
+    //self.mapView.delegate = self;
+    self.mapView.showsUserLocation = YES;
+    self.mapView.showsPointsOfInterest = YES;
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *loc = locations[0];
+    
+    // Center the map on the users current location
+    [self.mapView setRegion:MKCoordinateRegionMake(loc.coordinate, MKCoordinateSpanMake(0.06, 0.06))];
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse ||
+        status == kCLAuthorizationStatusAuthorizedAlways)
+    {
+        [self.locationManager requestLocation];
+    }
+    
 }
 
 
